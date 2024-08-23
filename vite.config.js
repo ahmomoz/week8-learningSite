d76dd91d596f52fe79d2ab8134c37e3ba0df256c
ahmomoz <x5426854268@gmail.com>
2024-08-23 20:29:59 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/week8-learning/',
+  base: '/week8-learningSite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
